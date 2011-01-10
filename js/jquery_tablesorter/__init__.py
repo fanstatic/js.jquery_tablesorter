e79f4fa94ceb0929a7c29863129b06c0e3f7e1c3
Jan-Jaap Driessen <devnull@localhost>
2011-01-10 22:27:54 +0100

    No space in name.

--- js/jquery_tablesorter/__init__.py
+++ js/jquery_tablesorter/__init__.py
@@ -1,7 +1,7 @@
 from fanstatic import Library, Resource
 from js.jquery import jquery
 
-library = Library('jquery tablesorter', 'resources')
+library = Library('jquery_tablesorter', 'resources')
 
 tablesorter = Resource(library, 'jquery.tablesorter.js',
     minified='jquery.tablesorter.min.js', depends=[jquery])
